6824b1b146e823f7ab6d25d8a1e88a4bf5acc765
shoaibrajper786 <144035150+shoaibrajper786@users.noreply.github.com>
2023-10-30 06:53:19 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// var arr = ["abc","123","true","xyz"]
+// for(i=0; i < arr.length;i++){
+//     console.log(arr[i])
+// }
+
+var userInput = +prompt ("enter a number to generate a multiplication");
+var tablelength = +prompt ("enter the table length");
+
+for ( var i = 1 ; i = tablelength; i++) {
+    document.write(userInput + "*"+ i + "="+ userInput * i +"br")
+}
+
+// var color = [ "pink","yellow","orange","green","purple"]
+
+// var user = prompt["ask the user what color he want to add at the end"]
+// color.pop("pink");
+
+// console.log(color)
